a91b08eb62a604068f5fe8a0976c1be8ebc48f46
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-10 15:33:25 -0600

    index page slider done gst

--- src/main/java/pupitre/ui/web/IndexController.java
+++ src/main/java/pupitre/ui/web/IndexController.java
@@ -6,6 +6,7 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.views.ModelAndView;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -13,8 +14,31 @@ import java.util.Map;
 public class IndexController {
   @Get
   public ModelAndView index(HttpRequest<?> request) {
+    var items = List.of(
+      Map.of(
+        "index", "rs-1706",
+        "image", "/images/nivo-1.jpg",
+        "text1", "Desarrollo de Microservicios",
+        "text2", "Cloud Native",
+        "caption", "Aprende los principios de Cloud Native y como aplicarlos <br> para desarrollar microservicios en Java con Spring Boot."
+      )/*,
+      Map.of(
+        "index", "rs-1707",
+        "image", "/images/nivo-2.jpg",
+        "text1", "Take the first step to",
+        "text2", "learn with us",
+        "caption", "Lorem ipsum dolor sit amet, consectetur elit, sed do <br> eiusmod tempor incididunt ut laboreet dolore magna aliqua."
+      ),
+      Map.of(
+        "index", "rs-1708",
+        "image", "/images/nivo-3.jpg",
+        "text1", "Are you ready to",
+        "text2", "apply?",
+        "caption", "Lorem ipsum dolor sit amet, consectetur elit, sed do <br> eiusmod tempor incididunt ut laboreet dolore magna aliqua."
+      )*/
+    );
     var dataToRender = Map.of(
-      "user", ""
+      "sliderItems", items
     );
 
     return new ModelAndView("index", dataToRender);
