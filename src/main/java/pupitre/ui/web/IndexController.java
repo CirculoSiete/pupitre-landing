3b803569232d844c2e776a07536ff65c1a8f23fe
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-10 17:28:40 -0600

    added popular courses

--- src/main/java/pupitre/ui/web/IndexController.java
+++ src/main/java/pupitre/ui/web/IndexController.java
@@ -14,33 +14,85 @@ import java.util.Map;
 public class IndexController {
   @Get
   public ModelAndView index(HttpRequest<?> request) {
-    var items = List.of(
-      Map.of(
-        "index", "rs-1706",
-        "image", "/images/nivo-1.jpg",
-        "text1", "Desarrollo de Microservicios",
-        "text2", "Cloud Native",
-        "caption", "Aprende los principios de Cloud Native y como aplicarlos <br> para desarrollar microservicios en Java con Spring Boot."
-      )/*,
-      Map.of(
-        "index", "rs-1707",
-        "image", "/images/nivo-2.jpg",
-        "text1", "Take the first step to",
-        "text2", "learn with us",
-        "caption", "Lorem ipsum dolor sit amet, consectetur elit, sed do <br> eiusmod tempor incididunt ut laboreet dolore magna aliqua."
-      ),
-      Map.of(
-        "index", "rs-1708",
-        "image", "/images/nivo-3.jpg",
-        "text1", "Are you ready to",
-        "text2", "apply?",
-        "caption", "Lorem ipsum dolor sit amet, consectetur elit, sed do <br> eiusmod tempor incididunt ut laboreet dolore magna aliqua."
-      )*/
-    );
     var dataToRender = Map.of(
-      "sliderItems", items
+      "sliderItems", sliderItems(),
+      "courses", popularCourses()
     );
 
     return new ModelAndView("index", dataToRender);
+  }
+
+  private List<Map<String, String>> sliderItems() {
+    return List.of(
+      slider(
+        "rs-1706",
+        "/images/nivo-1.jpg",
+        "Desarrollo de Microservicios",
+        "Cloud Native",
+        "Aprende los principios de Cloud Native y como aplicarlos <br> " +
+          "para desarrollar microservicios en Java con Spring Boot.") /*,
+      slider(
+        "rs-1707",
+        "/images/nivo-2.jpg",
+        "Take the first step to",
+        "learn with us",
+        "Lorem ipsum dolor sit amet, consectetur elit, sed do <br> " +
+          "eiusmod tempor incididunt ut laboreet dolore magna aliqua."
+      ),
+      slider(
+        "rs-1708",
+        "/images/nivo-3.jpg",
+        "Are you ready to",
+        "apply?",
+        "Lorem ipsum dolor sit amet, consectetur elit, sed do <br> " +
+          "eiusmod tempor incididunt ut laboreet dolore magna aliqua."
+      )*/
+    );
+  }
+
+  private Map<String, String> slider(String index, String image, String text1, String text2, String caption) {
+    return Map.of(
+      "index", index,
+      "image", image,
+      "text1", text1,
+      "text2", text2,
+      "caption", caption
+    );
+  }
+
+  private List<Map<String, String>> popularCourses() {
+    return List.of(
+      popularCourse(
+        "/images/courses/courses1-1.jpg",
+        "Mastering DevOps",
+        "En este curso aprenderás sobre todas las disciplinas y herramientas de la práctica de DevOps.",
+        "$8,000 + IVA",
+        "Virtual",
+        "#",
+        "#"
+      ),
+      popularCourse(
+        "/images/courses/courses1-2.jpg",
+        "Introducción a Micronaut",
+        "Conoce las bases fundamentales para desarrollar aplicaciones ligeras en Java",
+        "¡Gratis!",
+        "En línea-OnDemand",
+        "#",
+        "#"
+      )
+    );
+  }
+
+  private Map<String, String> popularCourse(String banner, String shortName, String shortDescription, String price,
+                                            String type, String detailsUrl, String registerUrl) {
+    return Map.of(
+      "banner", banner,
+      "shortName", shortName,
+      "shortDescription", shortDescription,
+      "price", price,
+      "type", type,
+      "detailsUrl", detailsUrl,
+      "registerUrl", registerUrl
+    );
   }
 }
